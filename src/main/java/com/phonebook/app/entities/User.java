1da704315ec5d76034606b48b109216508902a1f
lokendra-java <bannalokendra143@gmail.com>
2025-07-04 14:41:46 +0530

    Add User Entity Class that content information about user

---
+++ src/main/java/com/phonebook/app/entities/User.java
@@ -0,0 +1,38 @@
+package com.phonebook.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user_tab")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id",nullable = false)
+    private Integer userId;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name ="last_name",nullable = false)
+    private String lastName;
+    @Column(name="email",nullable = false,unique = true)
+    private String email;
+    @Column(name = "password",nullable = false)
+    private String password;
+    @Column(name = "confirm_password",nullable = false)
+    private String confirmPassword;
+    @Column(name = "username",nullable = false,unique = true)
+    private String username;
+}
